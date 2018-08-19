010d643590e3eae2ddec007db800bc0e3c2e9dd6
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-08-19 21:28:12 +0530

    removed comment saicharn trying

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ app.get('/*', (req,res)=>{
   res.sendFile(path.join(__dirname,'/dist/gandhiDarshan/index.html'));
 
 });
-//
-sai charans comment
-//
+
 app.listen(process.env.PORT || 8080, ()=>{
 console.log('Server started');
 })
